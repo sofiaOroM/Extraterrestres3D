182d9df6597268bc8ba0d65c053c0cb29b6b3bb1
Sofia Orozco <sofiamen288@gmail.com>
2026-09-18 19:34:36 -0600

    Expression: Clase de manejo de identificadores / variables

--- src/main/java/sofiaO/ast/expresion/IdentifierNode.java
+++ src/main/java/sofiaO/ast/expresion/IdentifierNode.java
@@ -1,29 +1,18 @@
 package sofiaO.ast.expresion;
 
-import sofiaO.ast.ASTNode;
 import sofiaO.ast.ASTVisitor;
+import sofiaO.ast.BaseNode;
+import sofiaO.ast.Expression;
 
-public class IdentifierNode implements ASTNode {
-    public int line;
-    public int column;
+/** Referencia simple a una variable por nombre (sin campos/índices/llamadas) */
+public class IdentifierNode extends BaseNode implements Expression {
     public String nombre;
-    public IdentifierNode(int line, int column, String nombre) {
-        this.line = line;
-        this.column = column;
+
+    public IdentifierNode(String nombre, int line, int column) {
+        super(line, column);
         this.nombre = nombre;
     }
 
-    public <T> T accept(ASTVisitor<T> v) {
-        return v.visit(this);
-    }
-
     @Override
-    public int getLine() {
-        return line;
-    }
-
-    @Override
-    public int getColumn() {
-        return column;
-    }
+    public <T> T accept(ASTVisitor<T> v) { return v.visit(this); }
 }
